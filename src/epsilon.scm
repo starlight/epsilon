f47ecc444c7e0bb8ee96375b1d9ca144c2c9df0d
Arizona Hanson <arizona@starlig.ht>
2023-02-05 12:54:42 -0500

    add ninja build file and program stubs

---
+++ src/epsilon.scm
@@ -0,0 +1,9 @@
+; Scheme entrypoint for bootstrap compiler
+(define-library
+  (epsilon core)
+  (import
+    scheme
+    r7rs
+    epsilon.machine)
+  (begin
+    (foo)))
---
+++ src/machine.scm
@@ -0,0 +1,7 @@
+(define-library
+  (epsilon machine)
+  (import scheme r7rs)
+  (export foo)
+  (begin
+    (define (foo)
+      (display ""))))
